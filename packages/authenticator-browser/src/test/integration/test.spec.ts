134f76c53e453691ef8cf32ee13a1ad67d42ec5c
jaxoncreed <jaxoncreed@gmail.com>
2019-12-21 21:26:35 -0500

    Added the browser repo

---
+++ packages/authenticator-browser/src/test/integration/test.spec.ts
@@ -0,0 +1,7 @@
+import Authenticator from '../../index'
+
+describe('Authenticator Node Integration Tests', () => {
+  it('Should integration test', () => {
+    const authenticator = Authenticator()
+  })
+})
