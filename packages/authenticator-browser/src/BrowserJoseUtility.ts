df2ae18857d00eb5a4f160f41ec87b68ee1e8a95
jaxoncreed <jaxoncreed@gmail.com>
2020-01-14 17:25:04 -0500

    Can generate an invalid signature

--- packages/authenticator-browser/src/BrowserJoseUtility.ts
+++ packages/authenticator-browser/src/BrowserJoseUtility.ts
@@ -31,8 +31,17 @@ export default class BrowserJoseUtility implements IJoseUtility {
     key: JWKECKey | JWKOKPKey | JWKRSAKey | JWKOctKey,
     options?: JoseJWT.SignOptions
   ): Promise<string> {
-    const convertedKey: string = (await JWK.asKey(key, ''))
-    return JWT.sign(payload, convertedKey, options as JWT.SignOptions)
+    const convertedKey: string = (await JWK.asKey(key, 'pem')).toPEM()
+    console.log('PAYLOAD!')
+    console.log(payload)
+    console.log('PEM')
+    console.log(convertedKey)
+    const signed = JWT.sign(payload, convertedKey, {
+      ...options as JWT.SignOptions
+    })
+    console.log('signed!')
+    console.log(signed)
+    return signed
   }
 
   async privateJWKToPublicJWK (jwk: JSONWebKey): Promise<JSONWebKey> {
