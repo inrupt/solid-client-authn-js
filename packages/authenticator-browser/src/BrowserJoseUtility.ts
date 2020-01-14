0cbfc6e0e36f2b9f05399c31c83409f79ebb7bf1
jaxoncreed <jaxoncreed@gmail.com>
2020-01-14 17:27:49 -0500

    Remove console log

--- packages/authenticator-browser/src/BrowserJoseUtility.ts
+++ packages/authenticator-browser/src/BrowserJoseUtility.ts
@@ -32,15 +32,9 @@ export default class BrowserJoseUtility implements IJoseUtility {
     options?: JoseJWT.SignOptions
   ): Promise<string> {
     const convertedKey: string = (await JWK.asKey(key, 'pem')).toPEM()
-    console.log('PAYLOAD!')
-    console.log(payload)
-    console.log('PEM')
-    console.log(convertedKey)
     const signed = JWT.sign(payload, convertedKey, {
       ...options as JWT.SignOptions
     })
-    console.log('signed!')
-    console.log(signed)
     return signed
   }
 
