ad22fb97cdaf728b272939fa8f88d7c7a2486e37
jaxoncreed <jaxoncreed@gmail.com>
2020-02-04 17:04:56 -0500

    Partial completion of IssuerConfigFetcher tests

--- packages/authenticator-core/__tests__/login/oidc/IssuerConfigFetcher.spec.ts
+++ packages/authenticator-core/__tests__/login/oidc/IssuerConfigFetcher.spec.ts
@@ -1,6 +1,41 @@
+import 'reflect-metadata'
+import FetcherMocks from '../../util/Fetcher.mock'
+import StorageRetrieverMocks from '../../util/StorageRetriever.mock'
+import StorageMocks from '../../util/Storage.mock'
+import IssuerConfigFetcher from '../../../src/login/oidc/IssuerConfigFetcher'
+
 /**
  * Test for IssuerConfigFetcher
  */
-it('trivial', () => {
-  expect(true).toBe(true)
+describe('IssuerConfigFetcher', () => {
+
+  function initMocks () {
+    const fetcherMocks = FetcherMocks()
+    const storageRetrieverMocks = StorageRetrieverMocks()
+    const storageMocks = StorageMocks()
+    return {
+      ...fetcherMocks,
+      ...storageRetrieverMocks,
+      ...storageMocks,
+      issuerConfigFetcher: new IssuerConfigFetcher(
+        fetcherMocks.FetcherMock(),
+        storageRetrieverMocks.StorageRetrieverMock(),
+        storageMocks.StorageMock()
+      )
+    }
+  }
+
+  // describe ('fetchConfig', () => {
+  //   it ('uses a valid locally stored configuration instead of fetching one', () => {
+
+  //   })
+  //   it ('fetches a valid configuration ')
+  // })
+
+  // describe ('issuerConfigSchema', () => {
+  //   const issuerConfigs:  = {
+
+  //   }
+
+  // })
 })
