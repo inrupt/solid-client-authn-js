5628cf76e7bdbf63a23f711c99352ed85bb015c8
Vincent <git@vincenttunru.com>
2020-02-11 14:10:44 +0100

    Unit test for the AggregateAuthenticatedFetcher

---
+++ packages/authenticator-core/__tests__/authenticatedFetch/AggregateAuthenticatedFetcher.spec.ts
@@ -0,0 +1,17 @@
+jest.mock('../../src/util/handlerPattern/AggregateHandler')
+
+// Required by TSyringe:
+import 'reflect-metadata'
+import AggregateAuthenticatedFetcher from '../../src/authenticatedFetch/AggregateAuthenticatedFetcher'
+import IAuthenticatedFetcher from '../../src/authenticatedFetch/IAuthenticatedFetcher'
+import AggregateHandler from '../../src/util/handlerPattern/AggregateHandler'
+
+describe('AggregateAuthenticatedFetcher', () => {
+  it('should pass injected fetchers to its superclass', () => {
+    new AggregateAuthenticatedFetcher(['Some fetcher'] as any as IAuthenticatedFetcher[])
+
+    expect((AggregateHandler as jest.Mock).mock.calls).toEqual([
+      [ ['Some fetcher'] ],
+    ])
+  })
+})
