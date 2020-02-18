e01b5a833e18c80ad363e9380ade5a04dac72456
jaxoncreed <jaxoncreed@gmail.com>
2020-02-18 07:38:34 -0500

    New planned sequence dialog

---
+++ packages/authenticator-core/__tests__/util/validateSchema.sepc.ts
@@ -0,0 +1,15 @@
+import validateSchema from '../../src/util/validateSchema'
+
+describe('validateSchema', () => {
+  it('validates a simple schema', () = {
+
+  })
+
+  it('throws an error when the schema throw error is on', () => {
+
+  })
+
+  it('does not throw and error when throw error is not on', () => {
+    
+  })
+})
