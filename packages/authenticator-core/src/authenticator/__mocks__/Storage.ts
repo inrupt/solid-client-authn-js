39aed1a2bb499e4b05a72345addb48d18da8c993
Vincent <git@vincenttunru.com>
2020-02-24 16:47:54 +0100

    Use StorageMock in the StorageRetrieve spec
    Note that I removed the wrapping of the object in jest.fn().

--- packages/authenticator-core/src/authenticator/__mocks__/Storage.ts
+++ packages/authenticator-core/src/authenticator/__mocks__/Storage.ts
@@ -1,3 +1,5 @@
+import IStorage from "../IStorage";
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function StorageMocks(response?: string | null) {
   const StorageMockGetResponse: string = response || '{ "cool": "string" }';
@@ -13,11 +15,11 @@ export default function StorageMocks(response?: string | null) {
     /* void */
   });
 
-  const StorageMock = jest.fn(() => ({
+  const StorageMock: jest.Mocked<IStorage> = {
     get: StorageMockGetFunction,
     set: StorageMockSetFunction,
     delete: StorageMockDeleteFunction
-  }));
+  };
 
   return {
     StorageMockGetResponse,
