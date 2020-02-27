22d7e2671009c5dc9394fa33e8a1c26ddc506f9a
Vincent <git@vincenttunru.com>
2020-02-27 10:34:08 +0100

    Apply Prettier to test files as well

--- packages/authenticator-node/src/storage/InMemoryStorage.ts
+++ packages/authenticator-node/src/storage/InMemoryStorage.ts
@@ -1,20 +1,20 @@
 /**
  * In memory storage compatible with NodeJS
  */
-import IStorage from '@solid/authenticator-core/dist/authenticator/IStorage'
+import IStorage from "@solid/authenticator-core/dist/authenticator/IStorage";
 
 export default class InMemoryStorage implements IStorage {
-  private map: { [key: string]: string }
+  private map: { [key: string]: string };
 
-  async get (key: string): Promise<string | undefined> {
-    return this.map[key]
+  async get(key: string): Promise<string | undefined> {
+    return this.map[key];
   }
 
-  async set (key: string, value: string): Promise<void> {
-    this.map[key] = value
+  async set(key: string, value: string): Promise<void> {
+    this.map[key] = value;
   }
 
-  async delete (key: string): Promise<void> {
-    delete this.map[key]
+  async delete(key: string): Promise<void> {
+    delete this.map[key];
   }
 }
