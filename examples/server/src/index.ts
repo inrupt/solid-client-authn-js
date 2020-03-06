a78b27b956584422ace71385010db178eeb77014
jaxoncreed <jaxoncreed@gmail.com>
2020-03-06 14:07:37 -0500

    Set up server demo layout

--- examples/server/src/index.ts
+++ examples/server/src/index.ts
@@ -1,8 +1,9 @@
-// import { login } from "solid-auth-fetcher";
+import { uniqueLogin } from "solid-auth-fetcher";
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 
 const PORT = 3001;
+const BASE_URL = `http://localhost:${PORT}`;
 
 const app = express();
 
@@ -14,9 +15,14 @@ app.get("/", (req: Request, res: Response) => {
   res.render("home");
 });
 
-app.post("/login", (req: Request, res: Response) => {
-  const webId: string = req.body.webId;
-  res.send();
+app.post("/login", async (req: Request, res: Response) => {
+  const webId: string = req.body.webid;
+  const redirect: string = await uniqueLogin({
+    webId,
+    redirect: `${BASE_URL}/redirect`
+  });
 });
+
+app.get("/redirect", (req: Request, res: Response) => {});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
