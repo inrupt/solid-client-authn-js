ea6c2efdf7c7b138c0bd398731106de6bc935ee4
jaxoncreed <jaxoncreed@gmail.com>
2020-03-17 17:56:27 -0400

    Removed extraneous code

--- src/solidSession/INeededAjaxAction.ts
+++
@@ -1,8 +0,0 @@
-import { RequestInfo, RequestInit } from "node-fetch";
-import INeededAction from "./INeededAction";
-
-export default interface INeededAjaxAction extends INeededAction {
-  actionType: "ajax";
-  requestInfo: RequestInfo;
-  requestInit: RequestInit;
-}
