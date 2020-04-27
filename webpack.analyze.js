4617a77d8f9b73d9680bc64261bea49446cd2d98
jaxoncreed <jaxoncreed@gmail.com>
2020-04-27 18:52:22 -0400

    Removed node-jose

--- webpack.analyze.js
+++ webpack.analyze.js
@@ -1,11 +1,7 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
-const webpackMerge = require("webpack-merge");
 
-const productionConfig = require("./webpack.prod.js");
-
-module.exports = () => {
-  return webpackMerge(productionConfig, {
-    plugins: [new BundleAnalyzerPlugin()]
-  });
+module.exports = {
+  mode: "production",
+  plugins: [new BundleAnalyzerPlugin()]
 };
