78b5d8feab282a32752d31483b1d6bf943b5168a
Nicolas Seydoux <nseydoux@inrupt.com>
2020-06-10 13:15:53 +0200

    Defaults to a browser fetch instead of `cross-fetch`
    If a browser fetch implementation is available, then it is used instead of `cross-fetch`. This is because cross-fetch uses a polyfill for fetch, which is incomplete and lacks some standard behaviour that is required in some of our use cases, such as reading responses as streams for LDflex.

--- src/util/Fetcher.ts
+++ src/util/Fetcher.ts
@@ -32,6 +32,9 @@ export interface IFetcher {
 export default class Fetcher implements IFetcher {
   async fetch(url: RequestInfo | URL, init?: RequestInit): Promise<Response> {
     const fetchUrl = url instanceof URL ? url.toString() : url;
+    if (typeof window !== undefined && typeof window.fetch !== undefined) {
+      return window.fetch(fetchUrl, init);
+    }
     return _fetch(fetchUrl, init);
   }
 }
