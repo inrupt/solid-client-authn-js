7351f11ba1e3b6b02dafd295d24a6e2984138d80
Nicolas Seydoux <nseydoux@inrupt.com>
2020-07-31 14:19:43 +0200

    Fix Androïd native example package casing

--- examples/single/native/android/app/src/main/java/com/solidauthfetcherreactnativedemo/MainActivity.java
+++ examples/single/native/android/app/src/main/java/com/solidauthfetcherreactnativedemo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.solidClientAuthnreactnativedemo;
+package com.solidClientAuthnReactNativeDemo;
 
 import com.facebook.react.ReactActivity;
 
--- examples/single/native/android/app/src/main/java/com/solidauthfetcherreactnativedemo/MainApplication.java
+++ examples/single/native/android/app/src/main/java/com/solidauthfetcherreactnativedemo/MainApplication.java
@@ -1,4 +1,4 @@
-package com.solidClientAuthnreactnativedemo;
+package com.solidClientAuthnReactNativeDemo;
 
 import android.app.Application;
 import android.content.Context;
