8265fac1a849ea3c84aff05850967670bfe97d22
Nicolas Seydoux <nseydoux@inrupt.com>
2020-08-19 15:55:29 +0200

    Document the Session class API

--- src/Session.ts
+++ src/Session.ts
@@ -27,6 +27,9 @@ import ClientAuthentication from "./ClientAuthentication";
 import { getClientAuthenticationWithDependencies } from "./dependencies";
 import { v4 } from "uuid";
 
+/**
+ * To initialize a Session instance, either both storages or clientAuthentication are required.
+ */
 export interface ISessionOptions {
   secureStorage: IStorage;
   insecureStorage: IStorage;
@@ -34,6 +37,9 @@ export interface ISessionOptions {
   clientAuthentication: ClientAuthentication;
 }
 
+/**
+ * A {@link Session} object represents a user's session on an application. The session holds state, as it stores information enabling acces to private resources after login for instance.
+ */
 export class Session extends EventEmitter {
   public readonly info: ISessionInfo;
   private clientAuthentication: ClientAuthentication;
@@ -71,6 +77,7 @@ export class Session extends EventEmitter {
 
   /**
    * Triggers the login process. Note that this method will redirect the user away from your app.
+   *
    * @param options Parameter to customize the login behaviour. In particular, two options are mandatory: `options.oidcIssuer`, the user's identity provider, and `options.redirectUrl`, the URL to which the user will be redirected after logging in their identity provider.
    * @returns This method should redirect the user away from the app: it does not return anything. The login process is completed by {@linkcode handleIncomingRedirect}.
    */
@@ -83,15 +90,29 @@ export class Session extends EventEmitter {
     this.emit("login");
   };
 
+  /**
+   * Fetches data using available login information. If the user is not logged in, this will behave as a regular `fetch`. The signature of this method is identical to the [canonical `fetch`](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API).
+   *
+   * @param url The URL from which data should be fetched.
+   * @param init Optional parameters customizing the request, by specifying an HTTP method, headers, a body, etc. Follows the [WHATWG Fetch Standard](https://github.github.io/fetch/).
+   */
   fetch = async (url: RequestInfo, init?: RequestInit): Promise<Response> => {
     return this.clientAuthentication.fetch(this.info.sessionId, url, init);
   };
 
+  /**
+   * Logs the user out of the application. This does not log the user out of the identity provider, and should not redirect the user away.
+   */
   logout = async (): Promise<void> => {
     await this.clientAuthentication.logout(this.info.sessionId);
     this.emit("logout");
   };
 
+  /**
+   * Completes the login process by receiving the information provided by the identity provider through redirect.
+   *
+   * @param url The URL where the user has been redirected, as information is given by the identity provider to the app through query strings.
+   */
   handleIncomingRedirect = async (
     url: string
   ): Promise<ISessionInfo | undefined> => {
@@ -105,10 +126,20 @@ export class Session extends EventEmitter {
     return sessionInfo;
   };
 
+  /**
+   * Register a callback function to be called when a user completes login.
+   *
+   * @param callback The function called when a user completes login.
+   */
   onLogin(callback: () => unknown): void {
     this.on("login", callback);
   }
 
+  /**
+   * Register a callback function to be called when a user logs out:
+   *
+   * @param callback The function called when a user completes logout.
+   */
   onLogout(callback: () => unknown): void {
     this.on("logout", callback);
   }
