d5e3df78270748a8e096fa4c8377a1bc41d8bfdb
pmcb55 <patm@inrupt.com>
2020-09-03 11:27:39 +0100

    Lerna setup, and ESLint, Jest and TS configs refactored

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
-{
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint", "prettier", "license-header"],
-  "extends": [
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier", "license-header"],
+  extends: [
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
     "plugin:prettier/recommended"
   ],
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
   },
-  "rules": {
+  rules: {
     "prettier/prettier": "error",
     "@typescript-eslint/interface-name-prefix": ["error", "always"],
     "import/no-unresolved": 0,
