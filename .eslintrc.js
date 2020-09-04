8a569fc6a221143be002a6104effcf7d381b0fe2
pmcb55 <patm@inrupt.com>
2020-09-04 17:46:11 +0100

    moved lerna bootstrap before lerna version

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,17 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     "prettier/prettier": "error",
     "@typescript-eslint/interface-name-prefix": ["error", "always"],
     "import/no-unresolved": 0,
-    // TODO: FIXME: This rule is causing 'Invalid license header' errors on all
-    //  the (valid!) source - so will have to look into this later!
     "license-header/header": ["error", "../../resources/license-header.js"],
-    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
-  }
-}
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  },
+};
