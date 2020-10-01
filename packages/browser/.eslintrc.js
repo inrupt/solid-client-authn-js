0698462a7a87fd76f1199aa9fc05774be932f821
pmcb55 <patm@inrupt.com>
2020-10-01 11:05:12 +0100

    Testing lint-staged...

--- packages/browser/.eslintrc.js
+++ packages/browser/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: ["../../.eslintrc.js"],
   rules: {
-    "license-header/header": ["error", "../../resources/license-header.js"],
+    // Disable license checking within our sub-package, as we expect it to be
+    // run from the mono-repo root instead (e.g. using 'lint-staged').
+    "license-header/header": ["off"],
   },
 };
