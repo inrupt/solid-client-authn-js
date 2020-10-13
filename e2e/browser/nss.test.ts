f8c6610e5ff603fb83f45eafbc3d2765bd3e4ab7
Vincent <Vinnl@users.noreply.github.com>
2020-10-13 11:32:11 +0200

    Base setup for TestCafe (#414)
    * Initial commit
    
    * Initial Upload
    
    * Include TestCafe configuration file.
    
    * NSS Login (#1)
    
    * NSS Login
    
    Included code of testing against NSS:
    - NSS Login
    - Fetch tests
    
    * More flexibility
    
    Updated code to:
    - Make it easier to adjust users.
    - Remove hardcodes from resources.
    
    * Move Kevin's TestCafe setup into a separate dir
    
    * Set up Kevin's TestCafe tests to run in CI
    
    This deploys the demo app to Vercel, and runs Kevin's TestCafe
    tests against it. Note that I've removed everything that would
    currently cause the tests to fail (the proper tests will be fixed
    up later), and that I've extracted variables into environment
    variables.
    
    Co-authored-by: Kevin Howard <kevinh@inrupt.com>

---
+++ e2e/browser/nss.test.ts
@@ -0,0 +1,43 @@
+import { nssLogin } from './helpers/login';
+
+// Page Models
+import fetchPage from './page-models/fetchPage';
+
+// Resources to test
+const resources = require('./resources.json');
+
+const applicationUrl = process.env.DEMO_URL ?? "https://solid-client-authn-js-atdw4zl4r.vercel.app/demo/";
+const nssPodServer = "inrupt.net";
+const nssUsername = process.env.E2E_NSS_USERNAME;
+const nssPassword = process.env.E2E_NSS_PASSWORD;
+
+// Fetch Tests
+fixture('Fetch tests - NSS')
+    //.disablePageCaching
+    //.disablePageReloads
+    .page(applicationUrl)
+    .beforeEach( async t => {
+
+        // Login
+        await nssLogin('https://' + nssPodServer, nssUsername, nssPassword);
+        await t.wait(10000);
+        await t.expect(fetchPage.fetchButton.exists).ok("Logged in");
+    });
+
+// Process all of the resources
+resources.forEach(data => {
+    test(`Resource: '${data.Name}'`, async t => {
+
+        // Form the URI of the resource to fetch
+        var webID = 'https://' + nssUsername + '.' + nssPodServer;
+        var fetchURI = data.URI.replace('<WEBID>', webID);
+
+        await t
+            .selectText(fetchPage.fetchURI)
+            .typeText(fetchPage.fetchURI, fetchURI)
+            .click(fetchPage.fetchButton)
+            .wait(2000)
+            .expect(fetchPage.fetchResponse.textContent).contains(data.AuthorisedResponse, "Authorized");
+
+    });
+});
