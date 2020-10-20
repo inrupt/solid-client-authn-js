827aaa65eb670bac7f12b55afb7d9589198b69fb
Pat McBennett <patm@inrupt.com>
2020-10-20 10:10:00 +0100

    Feat/add ess dev to e2e tests (#440)
    * WIP - waiting for ESS dev to come online
    
    * add ESS environments to test suite
    
    Will only attempt to test against Pod server that have environment
    variables created for them, so although ESS is not ready yet, we should
    only need to create the secrets in GitHub for test users in those
    environments once they are ready to test.

--- e2e/browser/testSuiteRunner.test.ts
+++ e2e/browser/testSuiteRunner.test.ts
@@ -19,18 +19,20 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import { Selector, t } from "testcafe";
 import FetchPage from "./page-models/FetchPage";
 
-import { essGluuLogin, nssLogin } from "./helpers/login";
+import { loginGluu, loginNss } from "./helpers/login";
 import ITestConfig from "./ITestConfig";
 import IPodServerConfig from "./IPodServerConfig";
+import { authorizeEss, authorizeNss } from "./helpers/authorizeClientApp";
 
 // Could probably provide this via a system environment variable too...
 const testSuite = require("./test-suite.json");
 
 // This is the deployed client application that we'll be using to exercise
 // various authentication scenarios. We expect the system environment value to
-// point at a deployed instance (e.g. an automatic Vercel deployment), but I
+// point at a deployed instance (e.g. an automated Vercel deployment), but I
 // don't think it makes sense to default to a hard-coded Vercel instance.
 // Instead, for running locally, it seems helpful to default to 'localhost'
 // instance.
@@ -39,69 +41,103 @@ const clientApplicationUrl =
 
 const testCafeWaitTime: string = process.env.E2E_TESTCAFE_WAIT_TIME ?? "1000";
 
-fixture(`Automated tests using client application: [${clientApplicationUrl}]`)
-  //.disablePageCaching
-  //.disablePageReloads
-  .page(clientApplicationUrl);
+fixture(
+  `Automated tests using client application: [${clientApplicationUrl}]`
+).page(clientApplicationUrl);
 
-async function performLogin(server: IPodServerConfig, testUserName: string) {
-  const testUserPassword = process.env[server.envTestUserPassword];
+async function selectBrokeredIdp(brokeredIdp: string) {
+  const selectIdp = await Selector("h2").withText(
+    "How would you like to login?"
+  ).exists;
 
-  switch (server.loginMechanism) {
-    case "essGluu":
-      await essGluuLogin(
-        server.identityProvider,
+  if (selectIdp) {
+    await t.click(`[alt=${brokeredIdp}]`);
+  }
+}
+
+async function performLogin(
+  podServerConfig: IPodServerConfig,
+  testUserName: string
+) {
+  const testUserPassword = process.env[podServerConfig.envTestUserPassword];
+
+  await selectBrokeredIdp(podServerConfig.brokeredIdp);
+
+  switch (podServerConfig.brokeredIdp) {
+    case "Gluu":
+      await loginGluu(
+        podServerConfig.identityProvider,
         testUserName,
-        testUserPassword,
-        parseInt(testCafeWaitTime, 10)
+        testUserPassword as string
       );
       break;
 
     case "nss":
-      await nssLogin(
-        server.identityProvider,
+      await loginNss(
+        podServerConfig.identityProvider,
         testUserName,
-        testUserPassword,
-        parseInt(testCafeWaitTime, 10)
+        testUserPassword as string
       );
       break;
 
     default:
       throw new Error(
-        `Unknown login mechanism in test suite configuration: [${server.loginMechanism}]`
+        `Unknown login mechanism in test suite configuration: [${podServerConfig.brokeredIdp}]`
       );
   }
+
+  switch (podServerConfig.authorizeClientAppMechanism) {
+    case "ess":
+      await authorizeEss();
+      break;
+
+    case "nss":
+      await authorizeNss();
+      break;
+
+    default:
+      throw new Error(
+        `Unknown login mechanism in test suite configuration: [${podServerConfig.authorizeClientAppMechanism}]`
+      );
+  }
+
+  await t.wait(parseInt(testCafeWaitTime, 10));
+  await t.expect(FetchPage.fetchButton.exists).ok("Logged in");
 }
 
 // Run through all the tests defined in our test suite.
 testSuite.podServerList.forEach((server: IPodServerConfig) => {
   const testUserName = process.env[server.envTestUserName] as string;
 
-  testSuite.testList.forEach((data: ITestConfig) => {
-    test(`Test: [${server.loginMechanism}] [${data.name}]`, async (t) => {
-      if (data.performLogin) {
-        await performLogin(server, testUserName);
-      }
+  if (!testUserName || testUserName.trim().length === 0) {
+      test(`Pod Server - IdP [${server.description}], no environment variable set for [${server.envTestUserName}], so skipping this server.`, async (t) => {});
+  } else {
+    testSuite.testList.forEach((data: ITestConfig) => {
+      test(`Pod Server - IdP [${server.description}], test [${data.name}]`, async (t) => {
+        if (data.performLogin) {
+          await performLogin(server, testUserName);
+        }
 
-      const podRoot = server.podResourceServer.replace(
-        "<TEST USER NAME>",
-        testUserName
-      );
-      const resourceToGet = data.resourceToGet.replace("<POD ROOT>", podRoot);
+        const podRoot = server.podResourceServer.replace(
+          "<TEST USER NAME>",
+          testUserName
+        );
+        const resourceToGet = data.resourceToGet.replace("<POD ROOT>", podRoot);
 
-      await t
-        .selectText(FetchPage.fetchUriTextbox)
-        .typeText(FetchPage.fetchUriTextbox, resourceToGet)
-        .click(FetchPage.fetchButton)
-        .wait(parseInt(testCafeWaitTime, 10));
+        await t
+          .selectText(FetchPage.fetchUriTextbox)
+          .typeText(FetchPage.fetchUriTextbox, resourceToGet)
+          .click(FetchPage.fetchButton)
+          .wait(parseInt(testCafeWaitTime, 10));
 
-      const responseBody = await FetchPage.fetchResponseTextbox.textContent;
+        const responseBody = await FetchPage.fetchResponseTextbox.textContent;
 
-      const expected = data.expectResponseContainsAnyOf.some((option) =>
-        responseBody.includes(option)
-      );
+        const expected = data.expectResponseContainsAnyOf.some((option) =>
+          responseBody.includes(option)
+        );
 
-      await t.expect(expected).ok();
+        await t.expect(expected).ok();
+      });
     });
-  });
+  }
 });
