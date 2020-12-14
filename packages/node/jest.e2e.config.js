bef9f6af0bc32c6397c7f0cd5cfa2aada1767278
Nicolas Seydoux <nseydoux@inrupt.com>
2020-12-14 12:03:57 +0100

    Aligned e2e jest config for jose

--- packages/node/jest.e2e.config.js
+++ packages/node/jest.e2e.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   ...require("../../jest.e2e.config"),
   testEnvironment: "node",
+  moduleNameMapper: {
+    // Here, the OIDC package is used, but any package depending on jose@3.x would do.
+    "^jose/(.*)$": "<rootDir>/node_modules/jose/dist/node/cjs/$1",
+  },
 };
