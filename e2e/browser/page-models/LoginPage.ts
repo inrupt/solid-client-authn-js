8cef16653832c1c766a36f41433c87dd034ae8bd
Zwifi <nseydoux@inrupt.com>
2021-01-09 00:44:01 +0100

    Rephrased e2e test description with debug hints (#928)
    Replay of d02a42a3e937b774287df00a6ba47c34da15ac3b without the
    package-lock updates.

--- e2e/browser/page-models/LoginPage.ts
+++ e2e/browser/page-models/LoginPage.ts
@@ -34,6 +34,12 @@ class LoginPage {
   }
 
   async submitLoginForm(identityServerUri: string) {
+    // If this select fails, it probably means our client application is not
+    // running (since all we're trying to do here is select the identity
+    // provider IRI textbox from a client application, so we can log into that
+    // provider).
+    // See our README, which recommends running 'demoClientApp' from the
+    // examples within our browser package.
     await t
       .selectText(this.identityProviderTextbox)
       .typeText(this.identityProviderTextbox, identityServerUri)
