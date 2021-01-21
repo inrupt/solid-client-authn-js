49c95893e4e34045a4dd92af60ed4993e66c2c45
Kay Kim <kay.kim@inrupt.com>
2021-01-21 11:55:04 -0500

    Docs: update copyright

--- packages/node/docs/api/conf.py
+++ packages/node/docs/api/conf.py
@@ -16,7 +16,10 @@
 
 # -- Project information -----------------------------------------------------
 
-copyright = '2020-present, Inrupt Inc.'
+import datetime
+
+copyright = u'{0} Inrupt Inc.'.format(datetime.date.today().year)
+
 
 # -- General configuration ---------------------------------------------------
 
