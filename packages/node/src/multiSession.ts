86846bf5bff858216c1a4d3bb843b0b00314b093
Nicolas Seydoux <nseydoux@inrupt.com>
2021-01-28 15:26:47 +0100

    Added TSDoc for NodeJS

--- packages/node/src/multiSession.ts
+++ packages/node/src/multiSession.ts
@@ -24,6 +24,23 @@ import ClientAuthentication from "./ClientAuthentication";
 import { getClientAuthenticationWithDependencies } from "./dependencies";
 import { defaultStorage, Session } from "./Session";
 
+/**
+ * Retrieve a Session from the given storage based on its session ID. If possible,
+ * the Session is logged in before it is returned, so that `session.fetch` may
+ * access private Resource without any additional interaction.
+ *
+ * If no storage is provided, a default in-memory storage will be used. It is
+ * instanciated once on load, and is shared across all the sessions. Since it
+ * is only available in memory, the storage is lost when the code stops running.
+ *
+ * A Session is available in storage as soon as it logged in once, and it is removed
+ * from storage on logout.
+ *
+ * @param sessionId The ID of the Session to retrieve
+ * @param storage The storage where the Session can be found
+ * @returns A session object, authenticated if possible, or undefined if no Session
+ * in storage matches the given ID.
+ */
 export async function getSessionFromStorage(
   sessionId: string,
   storage?: IStorage
@@ -53,6 +70,24 @@ export async function getSessionFromStorage(
   return session;
 }
 
+/**
+ * Retrieve the IDs for all the Sessions available in the given storage. Note that
+ * it is only the Session IDs that are returned, and not Session object. Given a
+ * Session ID, one may use [[getSessionFromStorage]] to get the actual Session
+ * object, while being conscious that logging in a Session required an HTTP
+ * interaction, so doing it in batch for a large number of sessions may result
+ * in performance issues.
+ *
+ * If no storage is provided, a default in-memory storage will be used. It is
+ * instanciated once on load, and is shared across all the sessions. Since it
+ * is only available in memory, the storage is lost when the code stops running.
+ *
+ * A Session is available in storage as soon as it logged in once, and it is removed
+ * from storage on logout.
+ *
+ * @param storage The storage where the Session can be found
+ * @returns An array of Session IDs
+ */
 export async function getSessionIdFromStorageAll(
   storage?: IStorage
 ): Promise<string[]> {
@@ -68,6 +103,20 @@ export async function getSessionIdFromStorageAll(
   return clientAuth.getSessionIdAll();
 }
 
+/**
+ * Clear the given storage from any existing Session ID. In order to remove an
+ * individual Session from storage, rather than going through this batch deletion,
+ * one may simply log the Session out calling `session.logout`.
+ *
+ * If no storage is provided, a default in-memory storage will be used. It is
+ * instanciated once on load, and is shared across all the sessions. Since it
+ * is only available in memory, the storage is lost when the code stops running.
+ *
+ * A Session is available in storage as soon as it logged in once, and it is removed
+ * from storage on logout.
+ *
+ * @param storage The storage where the Session can be found
+ */
 export async function clearSessionFromStorageAll(
   storage?: IStorage
 ): Promise<void> {
