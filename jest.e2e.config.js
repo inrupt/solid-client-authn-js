a32d63960c8029ebe7dafa41007c90e59fd8d072
Nicolas Seydoux <nseydoux@inrupt.com>
2021-04-15 16:12:32 +0200

    Extend jest e2e timeout

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,8 +2,9 @@ module.exports = {
   roots: ["<rootDir>"],
   clearMocks: true,
   // Because we're making HTTP requests that can take a while,
-  // tests should be given a little longer to complete:
-  testTimeout: 10000,
+  // tests should be given a little longer to complete. In particular
+  // using the NSS IdP.
+  testTimeout: 30000,
   testMatch: ["**/src/e2e/?(*.)+(spec|test).+(ts|tsx|js)"],
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
