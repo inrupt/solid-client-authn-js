3d0b4a7a691b23d1df034b44bc6832269dd2d899
Nicolas Seydoux <nseydoux@inrupt.com>
2021-04-20 10:24:07 +0200

    Inject handler when getting session from storage
    Users may get their session from the storage, and not from the
    constructor. In this case, since the handler isn't serialized with the
    session in storage, they'd have to provied the handler to the
    getSessionFromStorage function in the same way they have to provide it
    to the session constructor.

--- packages/node/src/multiSession.ts
+++ packages/node/src/multiSession.ts
@@ -43,7 +43,8 @@ import { defaultStorage, Session } from "./Session";
  */
 export async function getSessionFromStorage(
   sessionId: string,
-  storage?: IStorage
+  storage?: IStorage,
+  handleRefreshToken?: (token: string) => unknown
 ): Promise<Session | undefined> {
   const clientAuth: ClientAuthentication = storage
     ? getClientAuthenticationWithDependencies({
@@ -61,6 +62,7 @@ export async function getSessionFromStorage(
   const session = new Session({
     sessionInfo,
     clientAuthentication: clientAuth,
+    handleRefreshToken,
   });
   if (sessionInfo.refreshToken) {
     await session.login({
