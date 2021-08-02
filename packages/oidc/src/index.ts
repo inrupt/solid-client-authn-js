7fad5d3daf9093dca38e2646c646a4b8fc72a800
Nicolas Seydoux <nseydoux@inrupt.com>
2021-08-02 10:52:55 +0200

    Exports the refresh function from index

--- packages/oidc/src/index.ts
+++ packages/oidc/src/index.ts
@@ -48,6 +48,7 @@ export {
   TokenEndpointInput,
   CodeExchangeResult,
 } from "./dpop/tokenExchange";
+export { refresh } from "./refresh/refreshGrant";
 export {
   removeOidcQueryParam,
   clearOidcPersistentStorage,
