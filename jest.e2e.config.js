1ecfd64eb5c80f61d5acc96957dcc097d7404ef0
Nicolas Seydoux <nseydoux@inrupt.com>
2022-01-24 22:37:50 +0100

    Move Node e2e tests out of CI
    The end-to-end test will run in their own workflow. Additionally, this commit includes a couple of cleanup:
    - Fix typo in CI workflow
    - Exclude examples from code coverage
    - Rephrase comment in jest config
    - Rephrase comments in browser e2e tests

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   roots: ["<rootDir>"],
   clearMocks: true,
-  // Because we're making HTTP requests that can take a while,
-  // tests should be given a little longer to complete. In particular
-  // using the NSS IdP.
+  // Increase timeout to accomodate variable network latency
   testTimeout: 30000,
   testMatch: ["**/src/e2e/?(*.)+(spec|test).+(ts|tsx|js)"],
   transform: {
