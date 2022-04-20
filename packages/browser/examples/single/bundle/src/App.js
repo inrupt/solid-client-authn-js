14f6fc32dfd73e4562f67811e7e42011b051305f
Nicolas Seydoux <nseydoux@inrupt.com>
2022-04-20 16:08:22 +0200

    Document client identifier

--- packages/browser/examples/single/bundle/src/App.js
+++ packages/browser/examples/single/bundle/src/App.js
@@ -31,6 +31,9 @@ import {
 } from "@inrupt/solid-client-authn-browser";
 
 const REDIRECT_URL = "http://localhost:3113/";
+// This is the IRI where the Client identifier document (i.e. client-app-profile.jsonld)
+// is available to the OIDC issuer. See https://solid.github.io/solid-oidc/#clientids-document
+// for more information.
 const CLIENT_IDENTIFIER =
   "https://raw.githubusercontent.com/inrupt/solid-client-authn-js/main/packages/browser/examples/single/bundle/client-app-profile.jsonld";
 
