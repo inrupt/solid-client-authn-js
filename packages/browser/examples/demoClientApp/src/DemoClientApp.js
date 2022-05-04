c9b43934dc93f808ae5d52edf125f58d2aa2b9e3
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-05-04 22:13:40 +0200

    chore: use brokers with trailing slash in browser demoClientApp
    Without the trailing slash we end up with the broker configuration stored in local storage twice

--- packages/browser/examples/demoClientApp/src/DemoClientApp.js
+++ packages/browser/examples/demoClientApp/src/DemoClientApp.js
@@ -13,11 +13,11 @@ let identityProviderLogoutEndpointTimeout = null;
 const NSS_SERVER_URL = "https://inrupt.net/";
 
 const preconfiguedIdpList = [
-  "https://openid.dev-next.inrupt.com",
-  "https://broker.pod.inrupt.com",
-  "https://broker.dev-ess.inrupt.com",
-  "https://broker.demo-ess.inrupt.com",
-  "https://inrupt.net",
+  "https://openid.dev-next.inrupt.com/",
+  "https://broker.pod.inrupt.com/",
+  "https://broker.dev-ess.inrupt.com/",
+  "https://broker.demo-ess.inrupt.com/",
+  "https://inrupt.net/",
 ];
 
 const defaultIssuer = preconfiguedIdpList[1];
