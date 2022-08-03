99c7baeac9665ba1f62f1db25356fda8a561d6b6
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-08-03 13:38:34 +0200

    Chore: remove cd packaging (#2268)
    * ci: remove continuous delivery workflows
    
    These were poorly implemented and resulted in us publishing thousands of versions of our packages
    
    * ci: simplify release workflow
    
    We don't actually need to use deployments here

--- .github/workflows/cd-packaging-tests/node/commonjs.cjs
+++
@@ -1,9 +0,0 @@
-const { Session } = require("@inrupt/solid-client-authn-node");
-
-const session = new Session();
-console.log(session.info.sessionId);
-
-session.fetch("https://example.com").then(async (response) => {
-  const textContent = await response.text();
-  console.log("Fetched example.com:", textContent);
-});
