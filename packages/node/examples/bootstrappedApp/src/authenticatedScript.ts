82836b46f117d9c469c34276f25cc516aa552e22
Zwifi <nseydoux@inrupt.com>
2022-09-23 10:38:05 +0200

    Update bootstrapped app to static registration (#2346)
    The bootstrapped app was still relying on the now deprecated oidc-token-helper. It now instead uses static client registration, pointing to Inrupt's documentation for details.

--- packages/node/examples/bootstrappedApp/src/authenticatedScript.ts
+++ packages/node/examples/bootstrappedApp/src/authenticatedScript.ts
@@ -1,40 +1,22 @@
-import { Session } from "../../../dist/Session";
-import { FileSystemStorage } from "./fileSystemStorage";
+import { Session } from "@inrupt/solid-client-authn-node";
 
 const argv = require("yargs/yargs")(process.argv.slice(2))
   .describe("clientId", "The registered client ID.")
-  .alias("id", "clientId")
   .describe("clientSecret", "The secret associated with the client ID.")
-  .alias("secret", "clientSecret")
-  .describe(
-    "refreshToken",
-    "The OAuth refresh token (in our case, can be thought of as an API token)."
-  )
   .describe(
     "oidcIssuer",
     "The identity provider that issued the token (i.e. the OIDC issuer)."
   )
-  .alias("oidcIssuer", "issuer")
   .describe("resource", "The resource to fetch")
-  .demandOption(["oidcIssuer", "resource"])
+  .demandOption(["clientId", "clientSecret", "oidcIssuer", "resource"])
   .locale("en")
   .help().argv;
 
 async function main(): Promise<void> {
-  const storage = new FileSystemStorage("./session-data.json");
-  const session = new Session(
-    {
-      storage,
-      onNewRefreshToken: (newToken: string) => {
-        console.log(`New refresh token: [${newToken}]`);
-      },
-    },
-    "my-session"
-  );
+  const session = new Session();
   await session.login({
     clientId: argv.clientId,
     clientSecret: argv.clientSecret,
-    refreshToken: argv.refreshToken,
     oidcIssuer: argv.oidcIssuer,
   });
   if (session.info.isLoggedIn) {
