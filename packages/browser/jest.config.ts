24fa5357a376df5014ebc49d5a4cf5f3df4779df
Nicolas Seydoux <nseydoux@inrupt.com>
2022-10-19 16:25:12 +0200

    fixup! fixup! Make jest configs TS

--- packages/browser/jest.config.ts
+++ packages/browser/jest.config.ts
@@ -5,6 +5,8 @@ export default {
   ...baseConfig,
   // Allows running tests from packages/browser/
   setupFilesAfterEnv: ['<rootDir>/../../jest.setup.ts'],
+  // This test environment is an extension of jsdom. This module targets the
+  // browser environment only, so tests only need to run in jsdom. 
   // Currently, this is still required despite the polyfills in jest setup.
   // See comments in file.
   testEnvironment: "<rootDir>/../../tests/environment/customEnvironment.ts",
--- packages/oidc/jest.config.ts
+++ packages/oidc/jest.config.ts
@@ -3,6 +3,8 @@ import { JestConfigWithTsJest } from "ts-jest";
 
 export default {
   ...baseConfig,
+  // This test environment is an extension of jsdom. This module targets the
+  // browser environment only, so tests only need to run in jsdom.
   // Currently, this is still required despite the polyfills in jest setup.
   // See comments in file.
   testEnvironment: "<rootDir>/../../tests/environment/customEnvironment.ts",
