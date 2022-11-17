b805aad646bace903bd52f8d8e8acccce8cbdf63
Nicolas Seydoux <nseydoux@inrupt.com>
2022-11-17 12:35:52 +0100

    Fix non-injected global

--- e2e/node/e2e-test.spec.ts
+++ e2e/node/e2e-test.spec.ts
@@ -19,7 +19,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { it, describe, beforeEach, afterEach } from "@jest/globals";
+import { it, describe, beforeEach, afterEach, expect } from "@jest/globals";
 import { custom } from "openid-client";
 import {
   getNodeTestingEnvironment,
