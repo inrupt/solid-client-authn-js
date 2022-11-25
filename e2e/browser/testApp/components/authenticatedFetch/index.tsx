491baa3fb1a257d21b6954d4ad4c28bd89c98a9b
Nicolas Seydoux <nseydoux@inrupt.com>
2022-11-25 17:51:42 +0100

    Set up new test app

---
+++ e2e/browser/testApp/components/authenticatedFetch/index.tsx
@@ -0,0 +1,83 @@
+//
+// Copyright 2022 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import { useEffect, useState } from "react";
+import { getPodUrlAll } from "@inrupt/solid-client";
+import {
+  fetch as authenticatedFetch,
+  getDefaultSession,
+} from "@inrupt/solid-client-authn-browser";
+
+const session = getDefaultSession();
+
+export default function AuthenticatedFetch() {
+  const [resource, setResource] = useState<string>();
+  const [data, setData] = useState<string>();
+
+  const handleFetch = (e: any) => {
+    e.preventDefault();
+    if (resource !== undefined) {
+      authenticatedFetch(resource, {
+        headers: new Headers({ Accept: "text/turtle" }),
+      })
+        .then((response) => response.text())
+        .then(setData)
+        .catch((_e) => {
+          // FIXME add error status to the UI
+          console.error("Something went wrong");
+        });
+    }
+  };
+
+  useEffect(() => {
+    if (session.info.webId !== undefined) {
+      getPodUrlAll(session.info.webId, {
+        fetch: session.fetch,
+      })
+        .then((pods) => {
+          if (pods.length === 0) {
+            throw new Error("No pod root in webid profile");
+          }
+          setResource(pods[0]);
+        })
+        .catch((e) => {
+          // FIXME add error status to the UI
+          console.error("Something went wrong");
+        });
+    }
+  }, []);
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          value={resource}
+          onChange={(e) => {
+            setResource(e.target.value);
+          }}
+        />
+        <button onClick={(e) => handleFetch(e)}>Fetch</button>
+      </div>
+      <pre>{data}</pre>
+    </>
+  );
+}
