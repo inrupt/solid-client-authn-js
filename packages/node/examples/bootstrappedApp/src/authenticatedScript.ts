67074a430733760e05a55607c7569c50bfeb3f4d
Nicolas Seydoux <nseydoux@inrupt.com>
2022-12-16 11:35:49 +0100

    Lint

--- packages/node/examples/bootstrappedApp/src/authenticatedScript.ts
+++ packages/node/examples/bootstrappedApp/src/authenticatedScript.ts
@@ -1,6 +1,26 @@
+//
+// Copyright 2022 Inrupt Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 import { Session } from "@inrupt/solid-client-authn-node";
 
-const argv = require("yargs/yargs")(process.argv.slice(2))
+const { argv } = require("yargs/yargs")(process.argv.slice(2))
   .describe("clientId", "The registered client ID.")
   .describe("clientSecret", "The secret associated with the client ID.")
   .describe(
@@ -10,7 +30,7 @@ const argv = require("yargs/yargs")(process.argv.slice(2))
   .describe("resource", "The resource to fetch")
   .demandOption(["clientId", "clientSecret", "oidcIssuer", "resource"])
   .locale("en")
-  .help().argv;
+  .help();
 
 async function main(): Promise<void> {
   const session = new Session();
@@ -25,11 +45,12 @@ async function main(): Promise<void> {
       .then((response) => {
         return response.text();
       })
-      .then(console.log);
-    session.logout();
+      .then(console.log)
+      .then(() => session.logout())
+      .catch(console.error);
   } else {
     console.log("Not logged in.");
   }
 }
 
-main();
+main().catch(console.error);
