4300658f35cd8fa124b27bfb69ed4c3d4a36139b
Virginia Balseiro <virginia.balseiro@inrupt.com>
2023-01-12 10:53:41 +0100

    add support for RFC 9207 to node package

--- packages/node/src/login/oidc/incomingRedirectHandler/AuthCodeRedirectHandler.spec.ts
+++ packages/node/src/login/oidc/incomingRedirectHandler/AuthCodeRedirectHandler.spec.ts
@@ -441,6 +441,26 @@ describe("AuthCodeRedirectHandler", () => {
       );
     });
 
+    it("throws if the iss parameter does not match stored issuer", async () => {
+      const mockedStorage = mockDefaultRedirectStorage();
+      const mockedTokens = mockDpopTokens();
+      setupOidcClientMock(mockedTokens);
+      const authCodeRedirectHandler = getAuthCodeRedirectHandler({
+        storageUtility: mockedStorage,
+        sessionInfoManager: mockSessionInfoManager(mockedStorage),
+      });
+
+      await expect(
+        authCodeRedirectHandler.handle(
+          "https://my.app/redirect?code=someCode&state=someState&iss=someIssuer"
+        )
+      ).rejects.toThrow(
+        `The value of the iss parameter (someIssuer) does not match the issuer identifier of the authorization server (${
+          mockDefaultIssuerConfig().issuer
+        }). See [rfc9207](https://www.rfc-editor.org/rfc/rfc9207.html#section-2.3-3.1.1)`
+      );
+    });
+
     it("throws if the IdP does not return an access token", async () => {
       const mockedTokens = mockDpopTokens();
       mockedTokens.access_token = undefined;
