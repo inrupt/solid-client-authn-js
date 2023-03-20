b054d7e579856f00a416d389f41967843d8c4241
Nicolas Seydoux <nseydoux@inrupt.com>
2023-03-20 14:18:58 +0100

    Enforce alignment between on, once and off types

--- packages/core/src/SessionEventEmitter.ts
+++ packages/core/src/SessionEventEmitter.ts
@@ -25,6 +25,39 @@ export interface IHasSessionEventEmitter {
   events: ISessionEventEmitter;
 }
 
+// These types help preventing inconsistencies between on, once and off.
+type LOGIN_ARGS = { eventName: typeof EVENTS.LOGIN; listener: () => void };
+type LOGOUT_ARGS = { eventName: typeof EVENTS.LOGOUT; listener: () => void };
+type SESSION_EXPIRED_ARGS = {
+  eventName: typeof EVENTS.SESSION_EXPIRED;
+  listener: () => void;
+};
+type SESSION_RESTORED_ARGS = {
+  eventName: typeof EVENTS.SESSION_RESTORED;
+  listener: (currentUrl: string) => unknown;
+};
+type ERROR_ARGS = {
+  eventName: typeof EVENTS.ERROR;
+  listener: (error: string | null, errorDescription?: string | null) => unknown;
+};
+type SESSION_EXTENDED_ARGS = {
+  eventName: typeof EVENTS.SESSION_EXTENDED;
+  listener: (expiresIn: number) => void;
+};
+type TIMEOUT_SET_ARGS = {
+  eventName: typeof EVENTS.TIMEOUT_SET;
+  listener: (timeoutHandle: number) => void;
+};
+type NEW_REFRESH_TOKEN_ARGS = {
+  eventName: typeof EVENTS.NEW_REFRESH_TOKEN;
+  listener: (newToken: string) => void;
+};
+type FALLBACK_ARGS = {
+  eventName: Parameters<InstanceType<typeof EventEmitter>["on"]>[0];
+  // Prevents from using a SessionEventEmitter as an aritrary EventEmitter.
+  listener: never;
+};
+
 export interface ISessionEventEmitter extends EventEmitter {
   // The overloads should not be line-separated.
   /* eslint-disable lines-between-class-members */
@@ -33,27 +66,36 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param eventName The login event name
    * @param listener The callback called on successful login
    */
-  on(eventName: typeof EVENTS.LOGIN, listener: () => void): this;
+  on(
+    eventName: LOGIN_ARGS["eventName"],
+    listener: LOGIN_ARGS["listener"]
+  ): this;
   /**
    * Register a listener called on successful logout.
    * @param eventName The logout event name.
    * @param listener The callback called on successful logout.
    */
-  on(eventName: typeof EVENTS.LOGOUT, listener: () => void): this;
+  on(
+    eventName: LOGOUT_ARGS["eventName"],
+    listener: LOGOUT_ARGS["listener"]
+  ): this;
   /**
    * Register a listener called on session expiration.
    * @param eventName The session expiration event name.
    * @param listener The callback called on session expiration.
    */
-  on(eventName: typeof EVENTS.SESSION_EXPIRED, listener: () => void): this;
+  on(
+    eventName: SESSION_EXPIRED_ARGS["eventName"],
+    listener: SESSION_EXPIRED_ARGS["listener"]
+  ): this;
   /**
    * Register a listener called on session restoration after a silent login.
    * @param eventName The session restoration event name.
    * @param listener The callback called on successful session restore.
    */
   on(
-    eventName: typeof EVENTS.SESSION_RESTORED,
-    listener: (currentUrl: string) => unknown
+    eventName: SESSION_RESTORED_ARGS["eventName"],
+    listener: SESSION_RESTORED_ARGS["listener"]
   ): this;
   /**
    * Register a listener called on error, with an error identifier and description.
@@ -61,11 +103,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called on error.
    */
   on(
-    eventName: typeof EVENTS.ERROR,
-    listener: (
-      error: string | null,
-      errorDescription?: string | null
-    ) => unknown
+    eventName: ERROR_ARGS["eventName"],
+    listener: ERROR_ARGS["listener"]
   ): this;
   /**
    * Register a listener called on session extension.
@@ -73,8 +112,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called on session extension.
    */
   on(
-    eventName: typeof EVENTS.SESSION_EXTENDED,
-    listener: (expiresIn: number) => void
+    eventName: SESSION_EXTENDED_ARGS["eventName"],
+    listener: SESSION_EXTENDED_ARGS["listener"]
   ): this;
   /**
    * Register a listener called when a timeout is set for a session event with
@@ -83,8 +122,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called when setting a timeout.
    */
   on(
-    eventName: typeof EVENTS.TIMEOUT_SET,
-    listener: (timeoutHandle: number) => void
+    eventName: TIMEOUT_SET_ARGS["eventName"],
+    listener: TIMEOUT_SET_ARGS["listener"]
   ): this;
   /**
    * Register a listener called when a new refresh token is issued for the session.
@@ -92,12 +131,16 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called when a new refresh token is issued.
    */
   on(
-    eventName: typeof EVENTS.NEW_REFRESH_TOKEN,
-    listener: (newToken: string) => void
+    eventName: NEW_REFRESH_TOKEN_ARGS["eventName"],
+    listener: NEW_REFRESH_TOKEN_ARGS["listener"]
   ): this;
+  /**
+   * @hidden This is a fallback constructor overriding the EventEmitter behavior.
+   *  It shouldn"t be in the API docs.
+   */
   on(
-    eventName: Parameters<InstanceType<typeof EventEmitter>["on"]>[0],
-    listener: Parameters<InstanceType<typeof EventEmitter>["on"]>[1]
+    eventName: FALLBACK_ARGS["eventName"],
+    listener: FALLBACK_ARGS["listener"]
   ): this;
 
   /**
@@ -105,27 +148,36 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param eventName The login event name.
    * @param listener The callback called on the next successful login.
    */
-  once(eventName: typeof EVENTS.LOGIN, listener: (webid: string) => void): this;
+  once(
+    eventName: LOGIN_ARGS["eventName"],
+    listener: LOGIN_ARGS["listener"]
+  ): this;
   /**
    * Register a listener called on the next successful logout.
    * @param eventName The logout event name.
    * @param listener The callback called on the next successful logout.
    */
-  once(eventName: typeof EVENTS.LOGOUT, listener: () => void): this;
+  once(
+    eventName: LOGOUT_ARGS["eventName"],
+    listener: LOGOUT_ARGS["listener"]
+  ): this;
   /**
    * Register a listener called on the next session expiration.
    * @param eventName The session expiration event name.
    * @param listener The callback called on the next session expiration.
    */
-  once(eventName: typeof EVENTS.SESSION_EXPIRED, listener: () => void): this;
+  once(
+    eventName: SESSION_EXPIRED_ARGS["eventName"],
+    listener: SESSION_EXPIRED_ARGS["listener"]
+  ): this;
   /**
    * Register a listener called on the next session restoration after a silent login.
    * @param eventName The session restoration event name.
    * @param listener The callback called on the next successful session restore.
    */
   once(
-    eventName: typeof EVENTS.SESSION_RESTORED,
-    listener: (currentUrl: string) => unknown
+    eventName: SESSION_RESTORED_ARGS["eventName"],
+    listener: SESSION_RESTORED_ARGS["listener"]
   ): this;
   /**
    * Register a listener called on the next error, with an error identifier and description.
@@ -133,11 +185,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called on the next error.
    */
   once(
-    eventName: typeof EVENTS.ERROR,
-    listener: (
-      error: string | null,
-      errorDescription?: string | null
-    ) => unknown
+    eventName: ERROR_ARGS["eventName"],
+    listener: ERROR_ARGS["listener"]
   ): this;
   /**
    * Register a listener called on the next session extension.
@@ -145,8 +194,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called on the next session extension.
    */
   once(
-    eventName: typeof EVENTS.SESSION_EXTENDED,
-    listener: (expiresIn: number) => void
+    eventName: SESSION_EXTENDED_ARGS["eventName"],
+    listener: SESSION_EXTENDED_ARGS["listener"]
   ): this;
   /**
    * Register a listener called the next time a timeout is set for a session event
@@ -155,8 +204,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called when next setting a timeout.
    */
   once(
-    eventName: typeof EVENTS.TIMEOUT_SET,
-    listener: (timeoutHandle: number) => void
+    eventName: TIMEOUT_SET_ARGS["eventName"],
+    listener: TIMEOUT_SET_ARGS["listener"]
   ): this;
   /**
    * Register a listener called the next time a new refresh token is issued for
@@ -165,12 +214,16 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called next time a new refresh token is issued.
    */
   once(
-    eventName: typeof EVENTS.NEW_REFRESH_TOKEN,
-    listener: (newToken: string) => void
+    eventName: NEW_REFRESH_TOKEN_ARGS["eventName"],
+    listener: NEW_REFRESH_TOKEN_ARGS["listener"]
   ): this;
+  /**
+   * @hidden This is a fallback constructor overriding the EventEmitter behavior.
+   *  It shouldn"t be in the API docs.
+   */
   once(
-    eventName: Parameters<InstanceType<typeof EventEmitter>["on"]>[0],
-    listener: Parameters<InstanceType<typeof EventEmitter>["on"]>[1]
+    eventName: FALLBACK_ARGS["eventName"],
+    listener: FALLBACK_ARGS["listener"]
   ): this;
 
   /**
@@ -178,27 +231,36 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param eventName The login event name.
    * @param listener The callback to unregister.
    */
-  off(eventName: typeof EVENTS.LOGIN, listener: (webid: string) => void): this;
+  off(
+    eventName: LOGIN_ARGS["eventName"],
+    listener: LOGIN_ARGS["listener"]
+  ): this;
   /**
    * Unegister a listener called on successful logout.
    * @param eventName The logout event name.
    * @param listener The callback to unregister.
    */
-  off(eventName: typeof EVENTS.LOGOUT, listener: () => void): this;
+  off(
+    eventName: LOGOUT_ARGS["eventName"],
+    listener: LOGOUT_ARGS["listener"]
+  ): this;
   /**
    * Unegister a listener called on session expiration.
    * @param eventName The session expiration event name.
    * @param listener The callback to unregister.
    */
-  off(eventName: typeof EVENTS.SESSION_EXPIRED, listener: () => void): this;
+  off(
+    eventName: SESSION_EXPIRED_ARGS["eventName"],
+    listener: SESSION_EXPIRED_ARGS["listener"]
+  ): this;
   /**
    * Unegister a listener called on session restoration after a silent login.
    * @param eventName The session restoration event name.
    * @param listener The callback to unregister.
    */
   off(
-    eventName: typeof EVENTS.SESSION_RESTORED,
-    listener: (currentUrl: string) => unknown
+    eventName: SESSION_RESTORED_ARGS["eventName"],
+    listener: SESSION_RESTORED_ARGS["listener"]
   ): this;
   /**
    * Unegister a listener called on error, with an error identifier and description.
@@ -206,11 +268,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback to unregister.
    */
   off(
-    eventName: typeof EVENTS.ERROR,
-    listener: (
-      error: string | null,
-      errorDescription?: string | null
-    ) => unknown
+    eventName: ERROR_ARGS["eventName"],
+    listener: ERROR_ARGS["listener"]
   ): this;
   /**
    * Unegister a listener called on session extension.
@@ -218,8 +277,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback to unregister.
    */
   off(
-    eventName: typeof EVENTS.SESSION_EXTENDED,
-    listener: (expiresIn: number) => void
+    eventName: SESSION_EXTENDED_ARGS["eventName"],
+    listener: SESSION_EXTENDED_ARGS["listener"]
   ): this;
   /**
    * Unegister a listener called when a timeout is set for a session event.
@@ -227,8 +286,8 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called when next setting a timeout.
    */
   off(
-    eventName: typeof EVENTS.TIMEOUT_SET,
-    listener: (timeoutHandle: number) => void
+    eventName: TIMEOUT_SET_ARGS["eventName"],
+    listener: TIMEOUT_SET_ARGS["listener"]
   ): this;
   /**
    * Unegister a listener called when a new refresh token is issued.
@@ -236,11 +295,15 @@ export interface ISessionEventEmitter extends EventEmitter {
    * @param listener The callback called next time a new refresh token is issued.
    */
   off(
-    eventName: typeof EVENTS.NEW_REFRESH_TOKEN,
-    listener: (newToken: string) => void
+    eventName: NEW_REFRESH_TOKEN_ARGS["eventName"],
+    listener: NEW_REFRESH_TOKEN_ARGS["listener"]
   ): this;
+  /**
+   * @hidden This is a fallback constructor overriding the EventEmitter behavior.
+   *  It shouldn"t be in the API docs.
+   */
   off(
-    eventName: Parameters<InstanceType<typeof EventEmitter>["on"]>[0],
-    listener: Parameters<InstanceType<typeof EventEmitter>["on"]>[1]
+    eventName: FALLBACK_ARGS["eventName"],
+    listener: FALLBACK_ARGS["listener"]
   ): this;
 }
