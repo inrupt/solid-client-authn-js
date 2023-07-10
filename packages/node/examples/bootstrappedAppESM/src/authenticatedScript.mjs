cc3b7da37f0dd94dc62819e32644fbf82ae9a252
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-07-11 09:10:36 +1000

    feat: expose *-authn-node and *-authn-browser as ESM

--- packages/node/examples/bootstrappedAppESM/src/authenticatedScript.mjs
+++ packages/node/examples/bootstrappedAppESM/src/authenticatedScript.mjs
@@ -18,6 +18,39 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-module.exports = {
-  mode: "production",
-};
+import { Session } from "@inrupt/solid-client-authn-node";
+import yargs from "yargs";
+const { argv } = yargs(process.argv.slice(2))
+  .describe("clientId", "The registered client ID.")
+  .describe("clientSecret", "The secret associated with the client ID.")
+  .describe(
+    "oidcIssuer",
+    "The identity provider that issued the token (i.e. the OIDC issuer)."
+  )
+  .describe("resource", "The resource to fetch")
+  .demandOption(["clientId", "clientSecret", "oidcIssuer", "resource"])
+  .locale("en")
+  .help();
+
+async function main() {
+  const session = new Session();
+  await session.login({
+    clientId: argv.clientId,
+    clientSecret: argv.clientSecret,
+    oidcIssuer: argv.oidcIssuer,
+  });
+  if (session.info.isLoggedIn) {
+    session
+      .fetch(argv.resource)
+      .then((response) => {
+        return response.text();
+      })
+      .then(console.log)
+      .then(() => session.logout())
+      .catch(console.error);
+  } else {
+    console.log("Not logged in.");
+  }
+}
+
+main().catch(console.error);
--- packages/oidc-browser/rollup.config.mjs
+++ packages/oidc-browser/rollup.config.mjs
@@ -4,10 +4,9 @@
 // Until we only support Node 18+, this should be used instead
 // (see https://rollupjs.org/guide/en/#importing-packagejson) 
 import { createRequire } from 'node:module';
+import { createSharedConfig } from '../../rollup.common.mjs';
 const require = createRequire(import.meta.url);
 const pkg = require('./package.json');
-
-import typescript from "rollup-plugin-typescript2";
 
 export default {
   input: "./src/index.ts",
@@ -26,18 +25,5 @@ export default {
       name: "OidcDpopClient",
     },
   ],
-  plugins: [
-    typescript({
-      // Use our own version of TypeScript, rather than the one bundled with the plugin:
-      typescript: require("typescript"),
-      tsconfigOverride: {
-        compilerOptions: {
-          module: "esnext",
-        },
-      },
-    }),
-  ],
-  // external: ['crypto'],
-  // The following option is useful because symlinks are used in monorepos
-  preserveSymlinks: true,
+  ...createSharedConfig(pkg)
 };
