3278ce6d632b8496b60469ac4866cd385bbef03f
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-07-11 10:46:29 +1000

    feat: expose *-authn-node and *-authn-browser as ESM (#3049)

---
+++ packages/node/rollup.config.mjs
@@ -0,0 +1,11 @@
+// The following is only possible from Node 18 onwards
+// import pkg from "./package.json" assert { type: "json" };
+
+// Until we only support Node 18+, this should be used instead
+// (see https://rollupjs.org/guide/en/#importing-packagejson)
+import { createRequire } from "node:module";
+import createConfig from '../../rollup.common.mjs';
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
+
+export default createConfig(pkg);
