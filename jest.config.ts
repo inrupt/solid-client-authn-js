be92426144fe4b6722b9fe4985c516a2c6f6e556
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2023-08-25 13:31:47 +0200

    Return to ts-jest transformer default
    The dependencies that required this change now appear to have a CJS
    export, and the custom transformer setup caused issues with undici when
    upgrading TS.

--- jest.config.ts
+++ jest.config.ts
@@ -26,12 +26,7 @@ type ArrayElement<MyArray> = MyArray extends Array<infer T> ? T : never;
 const baseConfig: ArrayElement<NonNullable<Config["projects"]>> = {
   roots: ["<rootDir>"],
   testMatch: ["**/*.spec.ts"],
-  // This combination of preset/transformIgnorePatterns enforces that both TS and
-  // JS files are transformed to CJS, and that the transform also applies to the
-  // dependencies in the node_modules, so that ESM-only dependencies are supported.
-  preset: "ts-jest/presets/js-with-ts",
-  // deliberately set to an empty array to allow including node_modules when transforming code:
-  transformIgnorePatterns: [],
+  preset: "ts-jest",
   modulePathIgnorePatterns: ["dist/", "<rootDir>/examples/"],
   coveragePathIgnorePatterns: [".*.spec.ts", "dist/"],
   clearMocks: true,
