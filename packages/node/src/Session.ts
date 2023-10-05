e7024fe8f02f09d32ad04f558f5af50f24e6997f
Zach <zag0003@auburn.edu>
2023-10-05 02:58:04 -0500

    updated comment in internalLogout function of Session with explanation

--- packages/node/src/Session.ts
+++ packages/node/src/Session.ts
@@ -287,6 +287,9 @@ export class Session extends EventEmitter implements IHasSessionEventListener {
     if (emitEvent) {
       (this.events as EventEmitter).emit(EVENTS.LOGOUT);
       // I can't think of a better way to do this without reworking the events library
+      // I'm not sure how to emit partial events in Typescript the proper way
+      // eg -> login raises 0x10 and logout raises 0x01, LOGIN_AND_LOGOUT listens for both, while
+      // LOGIN listens for 0x10 and LOGOUT listens for 0x01
       (this.events as EventEmitter).emit(EVENTS.LOGIN_AND_LOGOUT);
     }
   };
