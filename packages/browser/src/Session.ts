c5152f6ab53e5df2a247e5f414dbd95f4d76a0e3
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2023-12-19 10:35:23 +0100

    Update API docs

--- packages/browser/src/Session.ts
+++ packages/browser/src/Session.ts
@@ -65,7 +65,8 @@ export interface IHandleIncomingRedirectOptions {
    * If your app's access has not expired yet and re-activation completed
    * successfully, a `sessionRestore` event will be fired with the URL the user
    * was at before they were redirected to their Solid Identity Provider.
-   * {@see onSessionRestore}
+   * {@see ISessionEventListener}: a callback can be registered to
+   * `session.events.on(SESSION_RESTORED, callback).
    */
   restorePreviousSession?: boolean;
 
