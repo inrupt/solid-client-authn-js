41aeed068ada78d552d40fbed4b5d8e174d899a0
Pete Edwards <edwardsph@users.noreply.github.com>
2024-02-20 16:34:04 +0000

    Remove CSS client credentials support

--- e2e/node/script/e2e-test.spec.ts
+++ e2e/node/script/e2e-test.spec.ts
@@ -51,15 +51,8 @@ if (process.env.CI === "true") {
 
 const ENV = getNodeTestingEnvironment();
 const { owner } = ENV.clientCredentials;
-const CSS = owner.type === "CSS Client Credentials";
 
 function getCredentials() {
-  if (CSS) {
-    throw new Error(
-      "CSS Does not support client credentials - these tests are not supported",
-    );
-  }
-
   return {
     clientId: owner.id,
     clientSecret: owner.secret,
@@ -67,165 +60,157 @@ function getCredentials() {
   };
 }
 
-(CSS ? describe.skip : describe)(
-  `End-to-end authentication tests for environment [${ENV.environment}}]`,
-  () => {
-    const authenticatedSession = new Session();
+describe(`End-to-end authentication tests for environment [${ENV.environment}}]`, () => {
+  const authenticatedSession = new Session();
 
-    // Log back in on session expiration
-    authenticatedSession.events.on("sessionExpired", () =>
-      authenticatedSession.login(getCredentials()),
-    );
-    beforeAll(() => authenticatedSession.login(getCredentials()));
+  // Log back in on session expiration
+  authenticatedSession.events.on("sessionExpired", () =>
+    authenticatedSession.login(getCredentials()),
+  );
+  beforeAll(() => authenticatedSession.login(getCredentials()));
 
-    // Making sure the session is logged out prevents tests from hanging due
-    // to the callback refreshing the access token.
-    afterAll(() => authenticatedSession.logout());
+  // Making sure the session is logged out prevents tests from hanging due
+  // to the callback refreshing the access token.
+  afterAll(() => authenticatedSession.logout());
 
-    // eslint-disable-next-line jest/expect-expect
-    it("test", async () => {
-      let headerResolver: (p: Headers) => void;
-      const headersPromise: Promise<Headers> = new Promise((resolve) => {
-        headerResolver = resolve;
-      });
-      await getSolidDataset("https://id.inrupt.com/zwifi", {
-        fetch: (info, init) =>
-          fetch(info, {
-            ...init,
-            headers: {
-              "some-custom-header": "custom-value",
-            },
-          }).then((response) => {
-            headerResolver(response.headers);
-            return response;
-          }),
-      });
-      const headers = await headersPromise;
-      console.log(headers.get("Content-Type"));
+  // eslint-disable-next-line jest/expect-expect
+  it("test", async () => {
+    let headerResolver: (p: Headers) => void;
+    await getSolidDataset("https://id.inrupt.com/zwifi", {
+      fetch: (info, init) =>
+        fetch(info, {
+          ...init,
+          headers: {
+            "some-custom-header": "custom-value",
+          },
+        }).then((response) => {
+          headerResolver(response.headers);
+          return response;
+        }),
+    });
+  });
+
+  describe("Authenticated fetch", () => {
+    it("properly sets up session information", async () => {
+      expect(authenticatedSession.info.isLoggedIn).toBe(true);
+      expect(authenticatedSession.info.sessionId).toBeDefined();
+      expect(authenticatedSession.info.webId).toBeDefined();
+      expect(authenticatedSession.info.expirationDate).toBeGreaterThan(
+        Date.now(),
+      );
     });
 
-    describe("Authenticated fetch", () => {
-      it("properly sets up session information", async () => {
-        expect(authenticatedSession.info.isLoggedIn).toBe(true);
-        expect(authenticatedSession.info.sessionId).toBeDefined();
-        expect(authenticatedSession.info.webId).toBeDefined();
-        expect(authenticatedSession.info.expirationDate).toBeGreaterThan(
-          Date.now(),
-        );
+    it("can fetch a public resource when logged in", async () => {
+      // FIXME: the WebID isn't necessarily a Solid resource.
+      const publicResourceUrl = authenticatedSession.info.webId as string;
+      const response = await authenticatedSession.fetch(publicResourceUrl, {
+        headers: {
+          Accept: "text/turtle",
+        },
       });
-
-      it("can fetch a public resource when logged in", async () => {
-        // FIXME: the WebID isn't necessarily a Solid resource.
-        const publicResourceUrl = authenticatedSession.info.webId as string;
-        const response = await authenticatedSession.fetch(publicResourceUrl, {
-          headers: {
-            Accept: "text/turtle",
-          },
-        });
-        expect(response.status).toBe(200);
-        await expect(response.text()).resolves.toContain(
-          authenticatedSession.info.webId,
-        );
-      });
-
-      it("can fetch a private resource when logged in", async () => {
-        // The following line doesn't compile because of the recursive dependency
-        // between the current package, the shared environment setup, and the
-        // published version of the current package.
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const privateResourceUrl = await getPodRoot(authenticatedSession);
-        const response = await authenticatedSession.fetch(privateResourceUrl, {
-          headers: {
-            Accept: "text/turtle",
-          },
-        });
-        expect(response.status).toBe(200);
-        // The root should contain at least one child
-        await expect(response.text()).resolves.toContain("contains");
-      });
-
-      it("can fetch a private resource when logged in after the same fetch failed", async () => {
-        const unauthSession = new Session();
-        // The following line doesn't compile because of the recursive dependency
-        // between the current package, the shared environment setup, and the
-        // published version of the current package.
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const privateResourceUrl = await getPodRoot(authenticatedSession);
-        let response = await unauthSession.fetch(privateResourceUrl);
-        expect(response.status).toBe(401);
-
-        await unauthSession.login(getCredentials());
-
-        response = await unauthSession.fetch(privateResourceUrl);
-        expect(response.status).toBe(200);
-
-        await unauthSession.logout();
-      });
+      expect(response.status).toBe(200);
+      await expect(response.text()).resolves.toContain(
+        authenticatedSession.info.webId,
+      );
     });
 
-    describe("Unauthenticated fetch", () => {
-      it("can fetch a public resource when not logged in", async () => {
-        // FIXME: This is only required to get a WebID. Once test setup is automated,
-        // this should be deleted.
-        const publicResourceUrl = authenticatedSession.info.webId as string;
-
-        const unauthenticatedSession = new Session();
-        const response = await unauthenticatedSession.fetch(publicResourceUrl, {
-          headers: {
-            Accept: "text/turtle",
-          },
-        });
-        expect(response.status).toBe(200);
-        await expect(response.text()).resolves.toContain(
-          authenticatedSession.info.webId,
-        );
+    it("can fetch a private resource when logged in", async () => {
+      // The following line doesn't compile because of the recursive dependency
+      // between the current package, the shared environment setup, and the
+      // published version of the current package.
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const privateResourceUrl = await getPodRoot(authenticatedSession);
+      const response = await authenticatedSession.fetch(privateResourceUrl, {
+        headers: {
+          Accept: "text/turtle",
+        },
       });
-
-      it("cannot fetch a private resource when not logged in", async () => {
-        const unauthenticatedSession = new Session();
-        // The following line doesn't compile because of the recursive dependency
-        // between the current package, the shared environment setup, and the
-        // published version of the current package.
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const privateResourceUrl = await getPodRoot(authenticatedSession);
-        const response = await unauthenticatedSession.fetch(privateResourceUrl);
-        expect(response.status).toBe(401);
-      });
+      expect(response.status).toBe(200);
+      // The root should contain at least one child
+      await expect(response.text()).resolves.toContain("contains");
     });
 
-    describe("Post-logout fetch", () => {
-      beforeAll(async () => authenticatedSession.logout());
+    it("can fetch a private resource when logged in after the same fetch failed", async () => {
+      const unauthSession = new Session();
+      // The following line doesn't compile because of the recursive dependency
+      // between the current package, the shared environment setup, and the
+      // published version of the current package.
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const privateResourceUrl = await getPodRoot(authenticatedSession);
+      let response = await unauthSession.fetch(privateResourceUrl);
+      expect(response.status).toBe(401);
 
-      it("can fetch a public resource after logging out", async () => {
-        // FIXME: the WebID isn't necessarily a Solid resource.
-        const publicResourceUrl = authenticatedSession.info.webId as string;
-        const response = await authenticatedSession.fetch(publicResourceUrl, {
-          headers: {
-            Accept: "text/turtle",
-          },
-        });
-        expect(response.status).toBe(200);
-        await expect(response.text()).resolves.toContain(
-          authenticatedSession.info.webId,
-        );
-      });
+      await unauthSession.login(getCredentials());
 
-      it("cannot fetch a private resource after logging out", async () => {
-        // The following line doesn't compile because of the recursive dependency
-        // between the current package, the shared environment setup, and the
-        // published version of the current package.
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const privateResourceUrl = await getPodRoot(authenticatedSession);
-        const response = await authenticatedSession.fetch(privateResourceUrl);
-        expect(response.status).toBe(401);
-      });
+      response = await unauthSession.fetch(privateResourceUrl);
+      expect(response.status).toBe(200);
+
+      await unauthSession.logout();
     });
-  },
-);
+  });
+
+  describe("Unauthenticated fetch", () => {
+    it("can fetch a public resource when not logged in", async () => {
+      // FIXME: This is only required to get a WebID. Once test setup is automated,
+      // this should be deleted.
+      const publicResourceUrl = authenticatedSession.info.webId as string;
+
+      const unauthenticatedSession = new Session();
+      const response = await unauthenticatedSession.fetch(publicResourceUrl, {
+        headers: {
+          Accept: "text/turtle",
+        },
+      });
+      expect(response.status).toBe(200);
+      await expect(response.text()).resolves.toContain(
+        authenticatedSession.info.webId,
+      );
+    });
+
+    it("cannot fetch a private resource when not logged in", async () => {
+      const unauthenticatedSession = new Session();
+      // The following line doesn't compile because of the recursive dependency
+      // between the current package, the shared environment setup, and the
+      // published version of the current package.
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const privateResourceUrl = await getPodRoot(authenticatedSession);
+      const response = await unauthenticatedSession.fetch(privateResourceUrl);
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe("Post-logout fetch", () => {
+    beforeAll(async () => authenticatedSession.logout());
+
+    it("can fetch a public resource after logging out", async () => {
+      // FIXME: the WebID isn't necessarily a Solid resource.
+      const publicResourceUrl = authenticatedSession.info.webId as string;
+      const response = await authenticatedSession.fetch(publicResourceUrl, {
+        headers: {
+          Accept: "text/turtle",
+        },
+      });
+      expect(response.status).toBe(200);
+      await expect(response.text()).resolves.toContain(
+        authenticatedSession.info.webId,
+      );
+    });
+
+    it("cannot fetch a private resource after logging out", async () => {
+      // The following line doesn't compile because of the recursive dependency
+      // between the current package, the shared environment setup, and the
+      // published version of the current package.
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const privateResourceUrl = await getPodRoot(authenticatedSession);
+      const response = await authenticatedSession.fetch(privateResourceUrl);
+      expect(response.status).toBe(401);
+    });
+  });
+});
 
 describe("Session events", () => {
   // These tests will check for session expiration, so they'll need a longer timeout.
