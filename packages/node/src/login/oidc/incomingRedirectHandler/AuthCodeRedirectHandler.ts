404ca63059a3a008c9b5c304de5a53064a7a9e2e
garciafdezpatricia <uo282210@uniovi.es>
2024-03-12 12:08:53 +0100

    with this change tests were passing locally with npm run test:e2e:node:all, checking the PR now

--- packages/node/src/login/oidc/incomingRedirectHandler/AuthCodeRedirectHandler.ts
+++ packages/node/src/login/oidc/incomingRedirectHandler/AuthCodeRedirectHandler.ts
@@ -129,11 +129,11 @@ export class AuthCodeRedirectHandler implements IIncomingRedirectHandler {
         clientInfo.clientType === "solid-oidc"
           ? undefined
           : clientInfo.clientSecret,
-      token_endpoint_auth_method:
-        clientInfo.clientType === "solid-oidc" ? "none" : "client_secret_basic",
-      // token_endpoint_auth_method: clientInfo.clientSecret
-      //   ? "client_secret_basic"
-      //   : "none",
+      // token_endpoint_auth_method:
+      //   clientInfo.clientType === "solid-oidc" ? "none" : "client_secret_basic",
+      token_endpoint_auth_method: clientInfo.clientSecret
+        ? "client_secret_basic"
+        : "none",
       id_token_signed_response_alg: clientInfo.idTokenSignedResponseAlg,
     });
 
